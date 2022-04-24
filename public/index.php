289fe2bbd5494a1f80bc48ab383d58ba6367f090
alftunner <alftunner@gmail.com>
2022-04-24 19:34:43 +0300

    Начал писать класс маршрутизации, закинул несколько маршрутов в таблицу маршрутов

--- public/index.php
+++ public/index.php
@@ -7,5 +7,7 @@ if (PHP_MAJOR_VERSION < 8) {
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \core\App();
