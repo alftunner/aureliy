2aa42d3e9a120c81be0ff0fb2a31db4ab34b7287
alftunner <alftunner@gmail.com>
2022-04-27 19:17:45 +0300

    продолжил работу над классом Router

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -24,7 +24,45 @@ class Router
 
     public static function dispatch($url)
     {
-        echo $url;
+        if (self::matchRoutes($url)) {
+            echo 'OK';
+        } else {
+            echo 'Not OK';
+        }
         debug(self::getRoutes());
+    }
+
+    public static function matchRoutes($url) : bool
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#", $url, $matches)) {
+                foreach ($matches as $key => $value) {
+                    if (is_string($key)) {
+                        $route[$key] = $value;
+                    }
+                }
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected static function upperCamelCase($name)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+    }
+
+    protected static function lowerCamelCase($name)
+    {
+        return lcfirst(self::upperCamelCase($name));
     }
 }
