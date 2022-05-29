efa1cb0000e1e1d878ca815fc92646cbbba6ec67
alftunner <alftunner@gmail.com>
2022-05-29 13:48:46 +0300

    5. Реализована возморжность прокидывать данные из контроллера в вид

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,7 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-
+        $this->setMeta('Главная страница', 'Описание главной страницы', 'Главная страница, мой фреймворк');
+        $this->setData(['test' => 'Тестовые данные']);
     }
 }
--- app/views/layouts/aureliy.php
+++ app/views/layouts/aureliy.php
@@ -8,7 +8,7 @@ use core\View;
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+    <?=$this->getMeta();?>
 </head>
 <body>
     <?= $this->content; ?>
